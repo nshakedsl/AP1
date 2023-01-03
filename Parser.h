49c14a8acd133ac314a26fd64febbf2f00e33343
OfirGurvits <ofirgurvits@gmail.com>
2023-01-03 21:03:53 +0200

    parser done

---
+++ Parser.h
@@ -0,0 +1,23 @@
+#ifndef AP1REAL_PARSER_H
+#define AP1REAL_PARSER_H
+
+#include "vector"
+#include <string>
+#include <vector>
+#include <ios>
+#include <iostream>
+#include <sstream>
+
+class Parser {
+    std::vector<std::string> components;
+public:
+    explicit Parser(const std::string& s);
+    bool validInput();
+    int getK();
+    std::string getDistance();
+    std::vector<double> getVector();
+
+};
+
+
+#endif //AP1REAL_PARSER_H
