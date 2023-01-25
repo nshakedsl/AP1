f98d9b54421ed3506e0fcea2b5b504695615502e
nshakedsl <shakedsolomon@gmail.com>
2023-01-25 20:10:45 +0200

    comments

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,6 +6,7 @@ std::string FileIO::read() {
     if (!myFile.is_open()) {
         throw std::invalid_argument("can't open file");
     }
+    // Read the entire file into a string
     std::string content((std::istreambuf_iterator<char>(myFile)), std::istreambuf_iterator<char>());
     return content;
 }
@@ -17,11 +18,13 @@ void FileIO::write(const std::string &x) {
     if (!myFile.is_open()) {
         throw std::invalid_argument("can't open file");
     }
+    // Write the string x to the file
     myFile << x;
     myFile.close();
 }
 
 void FileIO::setPath(std::string path) {
+    // set the fileName variable to the given path
     fileName = path;
 
 }
